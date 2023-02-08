f21879d2338c704bb8a22732118d91543628781d
Abdulrahman Rihan <bido4500@gmail.com>
2023-02-08 13:35:53 +0200

    final alterations

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const navBar = document.querySelector(".navigation");
+const gotoTop = document.querySelector(".goto-top");
+
+window.addEventListener("scroll", e => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navBar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix__nav");
+  } else {
+    navBar.classList.remove("fix__nav");
+  }
+
+  if (scrollHeight > 300) {
+    gotoTop.classList.add("show-top");
+  } else {
+    gotoTop.classList.remove("show-top");
+  }
+});
